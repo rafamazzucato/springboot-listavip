b1b6315f56846600b099ea08639f1b707c15a269
rafamazzucato <rafael.thomazelli@gmail.com>
2017-03-28 22:58:39 -0300

    create of first version to ListaVIP app

---
+++ src/main/java/br/com/alura/springboot/listavip/Configutarion.java
@@ -0,0 +1,26 @@
+package br.com.alura.springboot.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by rafaelthomazelli on 28/03/17.
+ */
+@SpringBootApplication
+@Controller
+public class Configutarion {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome(){
+        return "<h1>Olá. Bem-vindo ao sistema Lista VIP</h1>";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(Configutarion.class, args);
+    }
+
+}
