f8d4175d5c9ba5c192eaa76e13431c37c666c276
rafamazzucato <rafael.thomazelli@gmail.com>
2017-03-31 13:19:10 -0300

    Versão inicial exercicio 03

--- src/main/java/br/com/alura/springboot/listavip/controllers/IndexController.java
+++ src/main/java/br/com/alura/springboot/listavip/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package br.com.alura.springboot.listavip.controllers;
 
+import br.com.alura.springboot.listavip.model.Convidado;
 import br.com.alura.springboot.listavip.repository.ConvidadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by rafaelthomazelli on 30/03/17.
@@ -20,9 +23,19 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping("listaconvidados")
+    @RequestMapping("/listaconvidados")
     public String invitedlist(Model model){
         model.addAttribute("convidados", inviteRepo.findAll());
         return "invitedlist";
+    }
+
+    @RequestMapping(value= "save", method = RequestMethod.POST)
+    public String save(@RequestParam("nome") String nome, @RequestParam("email") String email,
+                       @RequestParam("telefone") String telefone, Model model){
+
+        Convidado convidado =  new Convidado(nome, email, telefone);
+        inviteRepo.save(convidado);
+
+        return this.invitedlist(model);
     }
 }
