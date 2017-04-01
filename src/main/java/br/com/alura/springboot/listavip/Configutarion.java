d346a38710efd6edab475ce43d1c1a387ed7d09e
rafamazzucato <rafael.thomazelli@gmail.com>
2017-03-31 22:26:14 -0300

    Desenvolvimento do Exercício 04 e 05

--- src/main/java/br/com/alura/springboot/listavip/Configutarion.java
+++ src/main/java/br/com/alura/springboot/listavip/Configutarion.java
@@ -2,10 +2,6 @@ package br.com.alura.springboot.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 /**
  * Created by rafaelthomazelli on 28/03/17.
@@ -17,14 +13,5 @@ public class Configutarion {
         SpringApplication.run(Configutarion.class, args);
     }
 
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
-        return dataSource;
-    }
 }
 
--- src/main/java/br/com/alura/springboot/listavip/controllers/IndexController.java
+++ src/main/java/br/com/alura/springboot/listavip/controllers/IndexController.java
@@ -1,13 +1,16 @@
 package br.com.alura.springboot.listavip.controllers;
 
+import br.com.alura.email.sender.EmailService;
 import br.com.alura.springboot.listavip.model.Convidado;
 import br.com.alura.springboot.listavip.repository.ConvidadoRepository;
+import br.com.alura.springboot.listavip.services.ConvidadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Created by rafaelthomazelli on 30/03/17.
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
 
     @Autowired
-    private ConvidadoRepository inviteRepo;
+    private ConvidadoService convidadoService;
 
     @RequestMapping("/")
     public String welcome(){
@@ -25,7 +28,7 @@ public class IndexController {
 
     @RequestMapping("/listaconvidados")
     public String invitedlist(Model model){
-        model.addAttribute("convidados", inviteRepo.findAll());
+        model.addAttribute("convidados", convidadoService.buscarTodos());
         return "invitedlist";
     }
 
@@ -34,7 +37,9 @@ public class IndexController {
                        @RequestParam("telefone") String telefone, Model model){
 
         Convidado convidado =  new Convidado(nome, email, telefone);
-        inviteRepo.save(convidado);
+        convidadoService.salvar(convidado);
+
+        new EmailService().send(nome, email);
 
         return this.invitedlist(model);
     }
